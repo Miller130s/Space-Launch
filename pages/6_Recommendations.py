c358abb5db00e5be5915b7350c3313e41602f498
Miller130s <nmiller1269@gmail.com>
2026-04-10 09:59:52 -0700

    n

--- pages/6_Recommendations.py
+++ pages/6_Recommendations.py
@@ -11,9 +11,7 @@ st.title("Recommendations")
 
 st.markdown(
 """ 
-- More launch sites in the future dedicated to heavy space launch payload missions
-- More people to support increase of missions (10 personnel minimum per mission per coast,
- double if 2 missions are happening at once)
+- More people to support increase of missions
  - Integrating the need for heavy-lift infrastructure
 - Maximize MAT Capacity and Readiness via Predictive Manpower Modeling
 
