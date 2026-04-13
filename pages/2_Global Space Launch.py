6d715848e382497a6d65488f34acc443adf70d39
Miller130s <nmiller1269@gmail.com>
2026-04-13 10:05:48 -0700

    n

--- pages/2_Global Space Launch.py
+++ pages/2_Global Space Launch.py
@@ -420,6 +420,6 @@ else:
                     tooltip={"html": "<b>Location:</b> {clean_loc}<br/><b>Count:</b> {launch_count}"}
                 ))
             
-            time.sleep(0.6)
+            time.sleep(0.9)
     else:
         st.info(f"Historical Data Found from {min_year}. Predictions through {max_year}. Click 'Start' to begin.")
