f012652494cb7116688d8a5d1080f0d6a2e1a039
Jake Bergen <jakerbergen@gmail.com>
2026-04-16 08:36:57 -0700

    m

--- pages/7_How To Contact Us.py
+++ pages/7_How To Contact Us.py
@@ -7,11 +7,11 @@ st.write("""
 If you have any questions, suggestions, or would like to get in touch with us, please feel free to reach out through the following channels:
 
          
--Jake Bergen
+Jake Bergen
 - Email: jakerbergen@gmail.com
 - Phone: +1 (402) 850-5755
          
--Nicholas Miller
+Nicholas Miller
 - Email: nmiller25@yahoo.com
 
 -GitHub link: https://github.com/Miller130s/Space-Launch
