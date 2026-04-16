0c67dc720d3e272fd2ff59cbe0288ef800132426
Jake Bergen <jakerbergen@gmail.com>
2026-04-16 08:34:17 -0700

    m

---
+++ pages/7_How To Contact Us.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.write("How to Contact Us")
+
+st.write("""
+If you have any questions, suggestions, or would like to get in touch with us, please feel free to reach out through the following channels: 
+Jake Bergen
+- Email: jakerbergen@gmail.com
+- Phone: +1 (402) 850-5755
+         
+Nicholas Miller
+- Email: nmiller25@yahoo.com
+""")
